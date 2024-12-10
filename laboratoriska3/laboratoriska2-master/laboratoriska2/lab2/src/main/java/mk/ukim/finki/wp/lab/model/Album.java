b6376eebbbfd5159481374648221b1abffa711d7
janaristevska <138867404+janaristevska@users.noreply.github.com>
2024-12-10 17:37:43 +0100

    Add files via upload

---
+++ laboratoriska3/laboratoriska2-master/laboratoriska2/lab2/src/main/java/mk/ukim/finki/wp/lab/model/Album.java
@@ -0,0 +1,36 @@
+package mk.ukim.finki.wp.lab.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String genre;
+
+    private String releaseYear;
+
+    @OneToMany(mappedBy = "album")
+    private List<Song> songs;
+
+    public Album(String name, String genre, String releaseYear) {
+        this.name = name;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    public Album() {
+
+    }
+}
